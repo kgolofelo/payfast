0e5014e73d46cbdfaa7ba5b90b7014897652b4be
Kgolofelo Selowa <kgolofelo@agileroutes.com>
2020-07-27 22:41:54 +0200

    Regulate maximum number of seats per film, per show time.

--- app/Http/Controllers/Booking.php
+++ app/Http/Controllers/Booking.php
@@ -18,20 +18,30 @@ class Booking extends Controller
     {
         try {
             $validator = Validator::make($request->all(), [
-                'user_id' => 'required|integer',
-                'film_id' => 'required|integer',
-                'film_show_time_id' => 'required|integer',
-                'cinema_location_id' => 'required|integer',
+                'film_id' => 'required|integer|min:1',
+                'film_show_time_id' => 'required|integer|min:1',
+                'cinema_location_id' => 'required|integer|min:1',
                 'number_of_seats' => 'required|integer',
             ]);
             if ($validator->fails()) {
                 return response()->json(['error' => $validator->errors()], 401);
             }
 
+            // Check how many seats are book per film, per show time
+            $bookedSeats = MovieBooking::where(['film_id' => $request->film_id, 'film_show_time_id' => $request->film_show_time_id])->sum('number_of_seats');
+
+            $newBookedSeats = $request->number_of_seats + $bookedSeats;
+            $maxSeats = 30;
+            if ($newBookedSeats > $maxSeats) {
+                return response()->json(['message' => sprintf('Sorry, there are %s seats available.',  ($maxSeats - $bookedSeats))], 200);
+            }
+
             $data = $request->all();
+            $data['user_id'] = $request->user()->id;
             $data['booking_reference'] = rand(0, 9999);
             $data['booking_status'] = 'accepted';
             if (MovieBooking::Create($data)) {
+                $data['message'] = sprintf('Booking reference: %s', $data['booking_reference']);
                 return response($data, 200);
             }
         } catch (\Exception $exception) {
